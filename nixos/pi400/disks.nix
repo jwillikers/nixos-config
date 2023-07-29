0d4c239ad1454c86ce64303b73b2fb437d2bfb83
Jordan Williams <jordan@jwillikers.com>
2023-07-29 13:55:31 -0500

    Fix pi400 disko

--- nixos/pi400/disks.nix
+++ nixos/pi400/disks.nix
@@ -7,53 +7,51 @@ in
     disk = {
       mmcblk0 = {
         type = "disk";
-        device = builtins.elemAt disks 0;
+        device = "/dev/disk/by-label/NIXOS_SD";
         content = {
-          type = "table";
-          format = "gpt";
-          partitions = [{
-            name = "ESP";
-            start = "0%";
-            end = "550MiB";
-            bootable = true;
-            flags = [ "esp" ];
-            fs-type = "fat32";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              start = "1M";
+              end = "550MiB";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
             };
-          }
-          {
-            name = "root";
-            start = "550MiB";
-            end = "100%";
-            content = {
-              type = "filesystem";
-              # Overwrite the existing filesystem
-              extraArgs = [ "-f" ];
-              format = "btrfs";
-              subvolumes = {
-                # Subvolume name is different from mountpoint
-                "/rootfs" = {
-                  mountpoint = "/";
-                  mountOptions = defaultBtrfsOpts;
-                };
-                # Subvolume name is the same as the mountpoint
-                "/home" = {
-                  mountOptions = defaultBtrfsOpts;
-                  mountpoint = "/home";
-                };
-                # Sub(sub)volume doesn't need a mountpoint as its parent is mounted
-                # "/home/user" = { };
-                # Parent is not mounted so the mountpoint must be set
-                "/nix" = {
-                  mountOptions = [ "defaults" "autodefrag" "commit=120" "compress=zstd" "noatime" ];
-                  mountpoint = "/nix";
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # Override existing partition
+                # Subvolumes must set a mountpoint in order to be mounted,
+                # unless their parent is mounted
+                subvolumes = {
+                  # Subvolume name is different from mountpoint
+                  "/rootfs" = {
+                    mountpoint = "/";
+                    mountOptions = defaultBtrfsOpts;
+                  };
+                  # Subvolume name is the same as the mountpoint
+                  "/home" = {
+                    mountOptions = defaultBtrfsOpts;
+                    mountpoint = "/home";
+                  };
+                  # Sub(sub)volume doesn't need a mountpoint as its parent is mounted
+                  # "/home/user" = { };
+                  # Parent is not mounted so the mountpoint must be set
+                  "/nix" = {
+                    mountOptions = [ "defaults" "autodefrag" "commit=120" "compress=zstd" "noatime" ];
+                    mountpoint = "/nix";
+                  };
                 };
               };
             };
-          }];
+          };
         };
       };
     };
